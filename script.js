d29dee5638ed0faa1ff4764f78a1a99dd0de8652
kimjihwanrun <jihwan010606@gmail.com>
2024-11-26 22:24:42 +0900

    변경 사항 설명

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ﻿// 마라톤 기록 데이터
 const marathons = [
-    { user: "kimjihwan", name: "서울 마라톤", year: "2024", category: "full", time: "4:35:12", distance: "42.195km", logo: "assets/logos/seoul-logo.png" },
+    { user: "kimjihwan", name: "2024 서울 마라톤", year: "2024", category: "full", time: "4:35:12", distance: "42.195km", logo: "assets/logos/seoul-logo.png" },
     { user: "kimjihwan", name: "부산 마라톤", year: "2023", category: "half", time: "2:10:45", distance: "21.1km", logo: "assets/logos/busan-logo.png" },
     { user: "kimjihwan", name: "제주 마라톤", year: "2024", category: "10k", time: "00:55:32", distance: "10km", logo: "assets/logos/jeju-logo.png" },
     { user: "kimsangjin", name: "서울 마라톤", year: "2024", category: "full", time: "5:00:00", distance: "42.195km", logo: "assets/logos/seoul-logo.png" },
